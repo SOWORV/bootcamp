95e7090e3b7488407e48178fafefa863e57be51d
SOWORV <ahmedshowrov32@gmail.com>
2019-06-17 23:22:05 -0400

    amazon test case

--- Amazon/src/test/java/testhomepage/TestHomePage.java
+++ Amazon/src/test/java/testhomepage/TestHomePage.java
@@ -1,4 +1,42 @@
 package testhomepage;
 
-public class TestHomePage {
+import base.CommonAPI;
+import homepage.HomePage;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestHomePage extends CommonAPI {
+
+    HomePage testhomepage;
+
+
+    String url ="https://www.amazon.com/";
+
+    @BeforeMethod
+    public void initialization(){
+        testhomepage = PageFactory.initElements(driver, HomePage.class);
+        driver.get(url);
+    }
+    /*@Test
+    public void clicksignIn(){
+        testhomepage.clicksignIn();
+        testhomepage.signin();
+
+    }
+    @Test
+    public void clickforgetpass(){
+        testhomepage.clicksignIn();
+        testhomepage.forgetpass();
+
+    }*/
+    @Test
+    public void searchbooks(){
+        testhomepage.purchase();
+    }
+
+
 }
